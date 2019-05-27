1961934ba064630672068605d2ff5a0da007f6fe
Austin Pritchett <austin@pritchett.info>
2019-05-27 16:38:05 -0500

    Add GPL v3 and documentation

--- src/MARIA.cpp
+++ src/MARIA.cpp
@@ -3,8 +3,22 @@
 // Author      : Austin Pritchett
 // Version     : 0.01
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Open Source re-implementation of the MARIE Simulator
 //============================================================================
+
+/*	This file is part of MARIA.
+
+    MARIA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    MARIA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MARIA.  If not, see <https://www.gnu.org/licenses/>.*/
 
 #include <iostream>
 using namespace std;

0ed609c0c9c05a74b22a54b62d0e17eb4bc5ce5a
Austin Pritchett <austin@pritchett.info>
2019-05-26 20:54:11 -0500

    Initial Commit

---
+++ src/MARIA.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MARIA.cpp
+// Author      : Austin Pritchett
+// Version     : 0.01
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
